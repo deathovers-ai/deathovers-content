6f1f690704b48896e3708ecbd48f610105ef9b80
deathovers-ai <deathovers.ai@gmail.com>
2026-07-07 22:15:59 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://deathovers.com',
-  output: 'static',
   integrations: [react()]
 });
